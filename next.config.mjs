4b841c0d048c75a6254b2207f3495553fe86e2c4
EBENEZER OLAYINKA ADENIYI <ebenezerolayinkaadeniyi@resnet300-320assiniboine-10-36-22-17.resnet.yorku.ca>
2026-07-16 17:07:52 -0400

    github action addition

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,24 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isProjectPages =
+  process.env.GITHUB_ACTIONS === 'true' &&
+  repoName &&
+  repoName !== `${process.env.GITHUB_REPOSITORY_OWNER}.github.io`;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  ...(isProjectPages
+    ? {
+        basePath: `/${repoName}`,
+        assetPrefix: `/${repoName}`,
+      }
+    : {}),
+};
 
 export default withNextIntl(nextConfig);
